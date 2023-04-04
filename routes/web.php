2728e9aec59835afa5c9eee06c8a510e0e7f9f67
Thariq <toriqthalib94@gmail.com>
2023-04-05 06:14:42 +0800

    menambahkan register page

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/',[HomeController::class, 'index'] );
 Route::get ('/login', function () {
     return view('login');
 });
+
+Route::get ('/register', function () {
+    return view('register');
+});
